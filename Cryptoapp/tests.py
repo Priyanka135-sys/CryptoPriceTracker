70768f9d79210620db8e4b27bf7c93f66e06bc69
Priyanka Gorkhe <priyankagorkhe08@gmail.com>
2024-01-08 02:06:31 +0530

    Final Test

--- Cryptoapp/tests.py
+++ Cryptoapp/tests.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from Cryptoapp.models import Cryptocurrency
 
-class YourAppTests(TestCase):
+class Cryptoapp(TestCase):
     
 
     def test_home_page(self):
@@ -17,4 +17,6 @@ class YourAppTests(TestCase):
         symbol = 'usd'  # Replace with a valid symbol for testing
         response = self.client.get(reverse('get_chart_data', args=[symbol]))
         self.assertEqual(response.status_code, 200)
+        
+        
        
