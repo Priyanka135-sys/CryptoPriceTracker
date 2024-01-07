ec2cfa5b6ac2a73953122a6c7225a00e993330f5
Priyanka Gorkhe <priyankagorkhe08@gmail.com>
2024-01-08 01:39:50 +0530

    Testing

--- Cryptoapp/tests.py
+++ Cryptoapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from Cryptoapp.models import Cryptocurrency
 
-# Create your tests here.
+class YourAppTests(TestCase):
+    
+
+    def test_home_page(self):
+        # Test the home page view
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        
+
+    def test_chart_data(self):
+       
+        symbol = 'usd'  # Replace with a valid symbol for testing
+        response = self.client.get(reverse('get_chart_data', args=[symbol]))
+        self.assertEqual(response.status_code, 200)
+       
